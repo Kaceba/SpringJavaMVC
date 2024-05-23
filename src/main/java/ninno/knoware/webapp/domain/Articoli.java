b839538a7eeb1a65e26c1fb374d160ec997c81b0
Ninno <>
2024-05-23 12:35:42 +0200

    added first db implementation, sql server

---
+++ src/main/java/ninno/knoware/webapp/domain/Articoli.java
@@ -0,0 +1,17 @@
+package ninno.knoware.webapp.domain;
+
+import java.io.Serializable;
+
+public class Articoli implements Serializable{
+
+    private String codArt;
+    private String descrizione;
+    private String um;
+    private String codStat;
+    private int pzCart;
+    private double pesoNetto;
+    private int idIva;
+    private String idStatoArt;
+    private Date dataCreaz;
+    private int idFamAss;
+}
