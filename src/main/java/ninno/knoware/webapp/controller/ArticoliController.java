dfaa537d6dbd8610eca86e7b81477ac9da653461
Ninno <>
2024-05-29 17:15:36 +0200

    small changes from last

--- src/main/java/ninno/knoware/webapp/controller/ArticoliController.java
+++ src/main/java/ninno/knoware/webapp/controller/ArticoliController.java
@@ -1,15 +1,18 @@
 package ninno.knoware.webapp.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import ninno.knoware.webapp.domain.Articoli;
 import ninno.knoware.webapp.service.ArticoliService;
@@ -39,16 +42,16 @@ public class ArticoliController {
         return "articoli";
     }
 
-
     /*
      * Example: http://localhost:8080/AlphaShop/articoli/cerca?filter=barilla&rep=1
-     * */
+     */
     @RequestMapping(value = "/cerca", method = RequestMethod.GET)
-    public String GetArticoliByFilter(@RequestParam("filter") String Filter, @RequestParam("rep") int IdRep, Model model) {
+    public String GetArticoliByFilter(@RequestParam("filter") String Filter, @RequestParam("rep") int IdRep,
+            Model model) {
 
         List<Articoli> recordset = articoliService.SelArticoliByFilter(Filter)
-            .stream()
-            .filter(u -> u.getIdFamAss() == IdRep).collect(Collectors.toList());
+                .stream()
+                .filter(u -> u.getIdFamAss() == IdRep).collect(Collectors.toList());
 
         if (recordset != null)
             NumArt = recordset.size();
@@ -59,6 +62,25 @@ public class ArticoliController {
         model.addAttribute("Articoli", recordset);
 
         return "articoli";
+    }
+
+    @RequestMapping(value = "/cerca/{filter}/{parametri}", method = RequestMethod.GET)
+    public String GetArticoliByFilterMatrix(@PathVariable("filter") String Filter,
+            @MatrixVariable(pathVar = "parametri") Map<String, List<String>> parametri, Model model) {
+
+        int NumArt = 0;
+        String orderBy = "codart";
+        String tipo = "desc";
+        Long SkipValue = (long) 0;
+        Long LimitValue = (long) 10;
+
+        List<String> IdRep = parametri.get("reparti");
+        List<String> OrderBy = parametri.get("OrderBy");
+        List<String> Paging = parametri.get("paging");
+
+        if(orderBy != null){
+
+        }
     }
 
 }
